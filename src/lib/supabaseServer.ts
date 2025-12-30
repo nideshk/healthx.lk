224dec0e439484c16cb04611e1d21fc3f954eb05
Anirudh Madhav Kulkarni <anirudhkulkarni9094@gmail.com>
2025-12-31 00:49:51 +0530

    fixing login and forgot password

--- src/lib/supabaseServer.ts
+++ src/lib/supabaseServer.ts
@@ -1,17 +1,20 @@
 import { cookies } from "next/headers";
 import { createClient } from "@supabase/supabase-js";
 
-export async function createServerSupabaseClient() {
+export async function supabaseServer() {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("sb-access-token")?.value;
+
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { 
-      auth : {persistSession: false},
+    {
+      auth: { persistSession: false },
       global: {
-        headers: {
-          cookie: cookies().toString(),
-        },
+        headers: accessToken
+          ? { Authorization: `Bearer ${accessToken}` }
+          : undefined,
       },
-     }
+    }
   );
 }
