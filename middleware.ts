7e2f0fccc57fb31e3ff3a798aed1a9f23584a15b
Anirudh Madhav Kulkarni <anirudhkulkarni9094@gmail.com>
2026-01-27 12:00:23 +0530

    local detection false

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   locales: ["en", "si"],
   defaultLocale: "en",
+  localeDetection: false, // 👈 THIS LINE
 });
 
 export const config = {
