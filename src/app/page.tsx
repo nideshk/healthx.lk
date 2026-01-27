079aa1e7841525309515970143fa27ff7e46fac4
Anirudh Madhav Kulkarni <anirudhkulkarni9094@gmail.com>
2026-01-27 12:24:44 +0530

    fix

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Body from "./(public)/body/body";
+import Footer from "@/components/homepage/footer/footer";
+
+export default function Page() {
+    return (
+        <div className="min-h-screen font-sans antialiased bg-gradient-to-br from-white via-cyan-100 to-cyan-50">
+            <Body />
+            <Footer />
+        </div>
+    );
+}
