d6b9140322dd2ba116e6181045ff74600ba77dca
Anirudh Madhav Kulkarni <anirudhkulkarni9094@gmail.com>
2026-01-27 12:46:43 +0530

    localePrefix never

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ import createMiddleware from "next-intl/middleware";
 export default createMiddleware({
   locales: ["en", "si"],
   defaultLocale: "en",
+  localePrefix: "never"
 });
 
 export const config = {
