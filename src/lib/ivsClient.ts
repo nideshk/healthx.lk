6c7956e403a2ff718b51b1d4eb813852cb1af4f4
Anirudh Madhav Kulkarni <anirudhkulkarni9094@gmail.com>
2026-02-01 01:44:55 +0530

    fixing build

--- src/lib/ivsClient.ts
+++ src/lib/ivsClient.ts
@@ -4,7 +4,7 @@ import { IVSRealTimeClient } from "@aws-sdk/client-ivs-realtime";
 export const ivsClient = new IVSRealTimeClient({
     region: process.env.AWS_REGION!,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID_IVS!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_IVS!,
     },
 });
