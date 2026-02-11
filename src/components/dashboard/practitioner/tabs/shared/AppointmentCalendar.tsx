501a62fefa56b1e83ead546d97e77d3e673d09b6
Psshr <psshr@gmail.com>
2026-02-11 11:16:12 +0530

    clinician fixes

--- src/components/dashboard/practitioner/tabs/shared/AppointmentCalendar.tsx
+++ src/components/dashboard/practitioner/tabs/shared/AppointmentCalendar.tsx
@@ -375,6 +375,9 @@ const DetailsModal: React.FC<DetailsModalProps> = ({
           <DetailRow label="Appointment Type" value={`${appointment.appointmentType}`} />
           <DetailRow label="Participants" value="1" />
           <DetailRow label="Reason" value={appointment.reason || "-"} />
+          <DetailRow label="email" value={appointment.email || "-"}/>
+          <DetailRow label="contact_number" value={appointment.contact_number || "-"}/>
+
           <DetailRow
             label="Status"
             value={appointment.status ? capitalize(appointment.status) : "-"}
